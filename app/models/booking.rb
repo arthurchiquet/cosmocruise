f5eb689e2cb9ef64d6e8de0abe2545d5596de80b
arthur chiquet <arthur.chiquet@gmail.com>
2023-06-21 11:25:28 +0200

    update models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :trip
   belongs_to :user
 
-  validates_presence_of :start_date, :end_date
+  validates_presence_of :start_date, :end_date, :nb_travelers
 end
